d27f770097683ae4822dcb5bd14632b3a26174ee
slowsly4r <khanh.vuongquoc@hcmut.edu.vn>
2025-09-17 00:55:45 +0700

    second commit

--- STM32-1/Core/Src/main.c
+++ STM32-1/Core/Src/main.c
@@ -86,14 +86,14 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-
+  int counter = 2;
+  // INITIAL STATE: LED RED ON
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  int counter = 2;
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, RESET);
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
   while (1)
   {
 	  if(counter <= 0) {
@@ -158,10 +158,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin LED_GREEN_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin;
+  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin */
+  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
